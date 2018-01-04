ea595270d169cf6a445621f086a8adcfffb91f70
Rori Jackson <rorijackson@Roris-MacBook-Pro.local>
2018-01-03 23:19:15 -0500

    adding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,35 +14,20 @@ var availGuesses = availWords[Math.floor(Math.random() * availWords.length)];
 
 function newGame() {
 	console.log(availGuesses);
-	
+
 	for (var i = 0; i < availGuesses.length; i++) {
 
 		underScores.push("_");
+
 	};
 	console.log(underScores);
-	
+
 	document.getElementById('letterSpaces').textContent = underScores.join(" ");
 
 }
 
-function reset() {
-	var numOfGuesses = 15;
-	var userChoices = [];
-	var underScores = [];
-	var wins = 0;
-	var losses = 0;
-	var badGuess = [];
-	var goodGuess = 0;
-	var availGuesses = availWords[Math.floor(Math.random() * availWords.length)];
-}
 
 newGame();
-reset();
-
-
-
-//grabbing the user's guesses
-
 
 document.onkeyup = function (event) {
 	userChoices = event.key.toLowerCase();
@@ -54,10 +39,13 @@ document.onkeyup = function (event) {
 		for (var i = 0; i < availGuesses.length; i++) {
 			if (availGuesses[i] === userChoices) {
 				underScores[i] = userChoices;
+				numOfGuesses--;
+				document.getElementById("numOfGuesses").innerHTML = numOfGuesses;
+				console.log(numOfGuesses);
 				document.getElementById('letterSpaces').innerHTML = underScores.join(" ");
-				
 				goodGuess++;
-				if (goodGuess === underScores) {
+			
+				if (goodGuess == underScores.length) {
 					alert("You win");
 					wins++;
 					document.getElementById('Wins').innerHTML = wins;
@@ -70,13 +58,14 @@ document.onkeyup = function (event) {
 		badGuess.push(userChoices);
 		numOfGuesses--;
 		document.getElementById("numOfGuesses").innerHTML = numOfGuesses;
-		if (numOfGuesses === 0) {
-			alert("You lose... Try again");
-			
-		}
 	}
-}
+	if (numOfGuesses === 0) {
+		losses++;
+		document.getElementById("losses").innerHTML = losses;
+		alert("You lose... Try again");
+	}
 
+}
 
 
 
